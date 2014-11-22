dd2c92d540d2d20da4322561e7f0b0ac730a5ccd
Bhawana Sharma <sharma46bhawana@gmail.com>
2014-11-21 17:26:59 -0800

    From windows

--- agent.py
+++ agent.py
@@ -30,16 +30,17 @@ def getClausesAndFacts():
             clauses.append(new_statement)
         else:
             facts.append(new_statement)
-    #print facts, clauses
-
-def getFunctionName(function):
-    goalFunctionName = function.split('(')[0]
-    return goalFunctionName
 
 def getArguments(function):
-    argument1 = ((function.split('(')[1]).split(')')[0]).split(',')[0]
-    argument2 = ((function.split('(')[1]).split(')')[0]).split(',')[1]
-    return argument1,argument2
+    if function.find(',') > 0:
+        argumentCount = 2
+        argument1 = ((function.split('(')[1]).split(')')[0]).split(',')[0]
+        argument2 = ((function.split('(')[1]).split(')')[0]).split(',')[1]
+    else:
+        argument1 = (function.split('(')[1]).split(')')[0]
+        argument2 = None
+        argumentCount = 1
+    return argumentCount, argument1,argument2
 
 def checkFacts(input):
     for fact in facts:
@@ -59,6 +60,58 @@ def conclusionMatch(input):
     else:
         return False
 
+def getAtomicPremises(resultArgument,clause):
+    premises = clause.split('=>')[0]
+    arrayOfPremises = premises.split('&')
+    for premise in arrayOfPremises:
+        substitu
+    return arrayOfPremises
+
+def checkForConclusion(result,conclusionFunction):
+    resultArgCount, resArg1, resArg2 = getArguments(result)
+    conclusionArgCount, conclusionArg1, conclusionArg2 = getArguments(conclusionFunction)
+
+    if resultArgCount == conclusionArgCount:
+        if resultArgCount == 1:
+            if resArg1 == conclusionArg1:
+                return True
+            elif conclusionArg1 == 'x':
+                return True
+            else:
+                return False
+        elif resultArgCount == 2:
+            if resArg1 == conclusionArg1 and resArg2 == conclusionArg2:
+                return True
+            elif resArg1 == conclusionArg1 and conclusionArg2 == 'x':
+                return True
+            elif resArg2 == conclusionArg2 and conclusionArg1 == 'x':
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
+def findSubstitution(result,conclusionFunction):
+    resultArgCount, resArg1, resArg2 = getArguments(result)
+    conclusionArgCount, conclusionArg1, conclusionArg2 = getArguments(conclusionFunction)
+
+    if resultArgCount == 1:
+        if resArg1 == conclusionArg1:
+            return None
+        elif conclusionArg1 == 'x':
+            return resArg1
+    elif resultArgCount == 2:
+        if resArg1 == conclusionArg1 and resArg2 == conclusionArg2:
+            return None
+        elif resArg1 == conclusionArg1 and conclusionArg2 == 'x':
+            return resArg2
+        elif resArg2 == conclusionArg2 and conclusionArg1 == 'x':
+            return resArg1
+
+
+
 
 def bcAlgorithm(result):
     print result
@@ -66,7 +119,19 @@ def bcAlgorithm(result):
         return True
     else:
         if conclusionMatch(result):
-            return True
+            resultFunctionName = result.split('(')[0]
+            for clause in clauses:
+                conclusionFunctionName = (clause.split('=>')[1]).split('(')[0]
+                conclusionFunction = clause.split('=>')[1]
+                if resultFunctionName == conclusionFunctionName:
+                    if checkForConclusion(result,conclusionFunction):
+                        constant = findSubstitution(result,conclusionFunction)
+                        if constant != None:
+
+
+
+        else:
+            return False
 
 if __name__ == '__main__':
     readFile()
