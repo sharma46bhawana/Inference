e47c5399214f3ef4a1aa799647c9fdd591382c15
Bhawana Sharma <sharma46bhawana@gmail.com>
2014-11-23 23:41:22 -0800

    code added

--- agent.py
+++ agent.py
@@ -1,5 +1,7 @@
 __author__ = 'Bhawana'
 
+import re
+
 # AI HW 3
 new_statements = []
 goal = ''
@@ -7,6 +9,7 @@ goalFunction = None
 statement_count = 0
 facts = []
 clauses = []
+#resultFlag = False
 
 def readFile():
     try:
@@ -112,19 +115,58 @@ def findSubstitution(result,conclusionFunction):
         elif resArg2 == conclusionArg2 and conclusionArg1 == 'x':
             return resArg1
 
-
-
-def something(a,clause):
+def getAtomicPremisesWithoutValue(clause):
     premises = clause.split('=>')[0]
     arrayOfPremises = premises.split('&')
-    newClauses = []
-    for premise in arrayOfPremises:
-        newClauses.append(premise.replace('x',a))
-    print newClauses
-    #print arrayOfPremises
+    return arrayOfPremises
+
+
+def sameConclusion(result,conclusionFunction):
+    resultArgCount, resArg1, resArg2 = getArguments(result)
+    conclusionArgCount, conclusionArg1, conclusionArg2 = getArguments(conclusionFunction)
+    if resultArgCount == conclusionArgCount:
+        if resultArgCount == 1:
+            if resArg1 == conclusionArg1:
+                return True
+            else:
+                return False
+        elif resultArgCount == 2:
+            if resArg1 == conclusionArg1 and resArg2 == conclusionArg2:
+                return True
+            else:
+                return False
+
+def checkInFacts(premise):
+    values = []
+    premiseArgCount, premiseArg1, premiseArg2 = getArguments(premise)
+    premiseFunctionName = premise.split('(')[0]
+    for fact in facts:
+        factArgCount,factArg1,factArg2 = getArguments(fact)
+        factFunctionName = fact.split('(')
+        if premise != fact:
+            if premiseFunctionName == factFunctionName:
+                if checkForConclusion(fact,premise):
+                    if premiseArgCount == 1:
+                        values.append((fact.split('(')[1]).split(')')[0])
+                    else:
+                        if premiseArg1 == factArg1 and premiseArg2 != factArg2:
+                            values.append((fact.split(',')[1]).split(')')[0])
+                        elif premiseArg2 == factArg2 and premiseArg1 != factArg1:
+                            values.append((fact.split(',')[0]).split('(')[1])
+
+    return values
+
+
+
+
+def bcAlgoWithoutSubstitution(premise):
+    values = []
+    values.append(checkInFacts(premise))
+
+
 
 def bcAlgorithm(result):
-    #print result
+    flag = False
     if checkFacts(result):
         return True
     else:
@@ -138,19 +180,26 @@ def bcAlgorithm(result):
                         constant = findSubstitution(result,conclusionFunction)
                         if constant != None:
                             arrayOfPremises = getAtomicPremises(constant,clause)
-                            print arrayOfPremises
                             for premise in arrayOfPremises:
                                 if bcAlgorithm(premise) == False:
-                                    return False
-                            return True
+                                    flag = False
+                                    break
+                                else:
+                                    flag = True
+                            if flag == True:
+                                return True
+                        else:
+                            if sameConclusion(result,conclusionFunction):
+                                premisesWithoutConstantValue = getAtomicPremisesWithoutValue(clause)
+                                values = bcAlgoWithoutSubstitution(premisesWithoutConstantValue[0])
+
+
+            if flag == False:
+                return False
         else:
             return False
 
 if __name__ == '__main__':
-    readFile()
-    #print goal, statement_count, new_statements
-    getClausesAndFacts()
-    #a = "John"
-    #something(a,clauses[1])
-    #something(a,clauses[2])
-    print bcAlgorithm(goal)
+    #readFile()
+    #getClausesAndFacts()
+    #print bcAlgorithm(goal)
